41047fb3d41756ddd390adbea8903cc3ecddce7c
DBryja <xileran@gmail.com>
2023-11-08 13:56:52 +0100

    Add ::after animation

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,15 +1,16 @@
 // import TextRotator from "./TextRotator";
 import gsap from "gsap";
-import { ScrollTrigger } from "gsap/all";
+import { ScrollTrigger, CSSRulePlugin } from "gsap/all";
 import SplitType from "split-type";
 import Lenis from "@studio-freight/lenis";
 import { useEffect } from "react";
 
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger, CSSRulePlugin);
 
 export default function AboutMe() {
   useEffect(() => {
     const splitTypes = document.querySelectorAll(".reveal-type");
+    const bryja = document.querySelector(".bryja");
     splitTypes.forEach((char, i) => {
       const text = new SplitType(char as HTMLElement, { types: "chars,words" });
       gsap.from(text.chars, {
@@ -19,11 +20,21 @@ export default function AboutMe() {
           end: "top 65%",
           scrub: true,
         },
-        y: 100,
+        y: -100,
         opacity: 0,
         stagger: 0.1,
       });
     });
+
+    gsap.from(bryja, {
+      scrollTrigger: {
+        trigger: bryja,
+        start: "top 90%",
+      },
+      "--opacity": 0,
+      duration: 1,
+    });
+
     const lenis = new Lenis();
     lenis.on("scroll", () => {
       ScrollTrigger.update();
@@ -42,7 +53,7 @@ export default function AboutMe() {
         <span className="reveal-type">Hi! My name is</span>
         <span className="bryja reveal-type">Dawid Bryja.</span>
       </h1>
-      <h2 className="reveal-type">I am building things for web.</h2>
+      <h2 className="reveal-type test">I am building things for web.</h2>
     </div>
   );
 }
